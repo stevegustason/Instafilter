be6e9e7f50dc3c7e158dcc99e9f0eaf389a92d75
stevegustason <steve.gustason@gmail.com>
2023-05-01 21:05:19 -0500

    Initial Commit

---
+++ Instafilter/InstafilterApp.swift
@@ -0,0 +1,17 @@
+//
+//  InstafilterApp.swift
+//  Instafilter
+//
+//  Created by Steven Gustason on 5/1/23.
+//
+
+import SwiftUI
+
+@main
+struct InstafilterApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
