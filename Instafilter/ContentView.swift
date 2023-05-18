2fd23311a7306ff208837489b6759aa0021c526d
stevegustason <steve.gustason@gmail.com>
2023-05-18 16:21:06 -0500

    Add functionality for changing filters and saving our photos.

--- Instafilter/ContentView.swift
+++ Instafilter/ContentView.swift
@@ -19,12 +19,33 @@ struct ContentView: View {
     // Property that will store the image the user selected
     @State private var inputImage: UIImage?
     // Set our current core image filter
-    @State private var currentFilter = CIFilter.sepiaTone()
+    @State private var currentFilter: CIFilter = CIFilter.sepiaTone()
     // Create our context
     let context = CIContext()
+    // Controls whether filter confirmation dialgoue is displayed
+    @State private var showingFilterSheet = false
+    // Store our temporary processed image that the user may want to save
+    @State private var processedImage: UIImage?
     
-    // Method that is called when the user clicks the save button. It will save the photo in its current state to the user's image library
+    // Method that is called when the user clicks the save button. It will save the photo in its current state to the user's image library using our ImageSaver class method
     func save() {
+        guard let processedImage = processedImage else { return }
+
+        // Create an instance of ImageSaver
+        let imageSaver = ImageSaver()
+
+        // Define our success message
+        imageSaver.successHandler = {
+            print("Success!")
+        }
+
+        // Define our error message
+        imageSaver.errorHandler = {
+            print("Oops: \($0.localizedDescription)")
+        }
+
+        // Call our writeToPhotoAlbum method
+        imageSaver.writeToPhotoAlbum(image: processedImage)
     }
     
     // Method that is called when the ImagePicker is dismissed.
@@ -38,17 +59,28 @@ struct ContentView: View {
     }
     
     func applyProcessing() {
-        // Set our filter's intensity based on the filterIntensity slider value
-        currentFilter.intensity = Float(filterIntensity)
+        // Set our filter's settings based on the filterIntensity slider value - because we have multiple different filter options, we need to use setValue(:_forKey:). Also, not all filters have an intensity option, so we first check which settings are available and if they exist for that filter, then we will set them
+        let inputKeys = currentFilter.inputKeys
+
+        if inputKeys.contains(kCIInputIntensityKey) { currentFilter.setValue(filterIntensity, forKey: kCIInputIntensityKey) }
+        if inputKeys.contains(kCIInputRadiusKey) { currentFilter.setValue(filterIntensity * 200, forKey: kCIInputRadiusKey) }
+        if inputKeys.contains(kCIInputScaleKey) { currentFilter.setValue(filterIntensity * 10, forKey: kCIInputScaleKey) }
 
         // Read the output image back from the filter
         guard let outputImage = currentFilter.outputImage else { return }
 
-        // Ask our CIContext to render it, then place the result into our image property so it’s visible on-screen
+        // Ask our CIContext to render it, then place the result into our image property so it’s visible on-screen. we'll also store the image in our processedImage property
         if let cgimg = context.createCGImage(outputImage, from: outputImage.extent) {
             let uiImage = UIImage(cgImage: cgimg)
             image = Image(uiImage: uiImage)
+            processedImage = uiImage
         }
+    }
+    
+    // Method to set our filter to the selected filter, then re-load our image using that new filter
+    func setFilter(_ filter: CIFilter) {
+        currentFilter = filter
+        loadImage()
     }
     
     var body: some View {
@@ -88,7 +120,7 @@ struct ContentView: View {
                 // And finally two buttons - one to change which core filter is applied, and the other to save the photo to the user's image library
                 HStack {
                     Button("Change Filter") {
-                        // change filter
+                        showingFilterSheet = true
                     }
                     
                     Spacer()
@@ -103,6 +135,18 @@ struct ContentView: View {
             // When the onTapGesture is triggered to update our showingImagePicker variable, we'll show an ImagePicker bound to inputImage
             .sheet(isPresented: $showingImagePicker) {
                 ImagePicker(image: $inputImage)
+            }
+            // Confirmation dialog works identically to alert - provide a title and condition to monitor, and as soon as the condition becomes true the confirmation dialog will be shown
+            .confirmationDialog("Select a filter", isPresented: $showingFilterSheet) {
+                // Here we  have a bunch of buttons to set the filter to different options, and a cancel button
+                Button("Crystallize") { setFilter(CIFilter.crystallize()) }
+                Button("Edges") { setFilter(CIFilter.edges()) }
+                Button("Gaussian Blur") { setFilter(CIFilter.gaussianBlur()) }
+                Button("Pixellate") { setFilter(CIFilter.pixellate()) }
+                Button("Sepia Tone") { setFilter(CIFilter.sepiaTone()) }
+                Button("Unsharp Mask") { setFilter(CIFilter.unsharpMask()) }
+                Button("Vignette") { setFilter(CIFilter.vignette()) }
+                Button("Cancel", role: .cancel) { }
             }
         }
     }
--- Instafilter/ImageSaver.swift
+++ Instafilter/ImageSaver.swift
@@ -9,11 +9,18 @@ import SwiftUI
 
 // Class that inherits from NSObject. Inside there we need a method with a precise signature that’s marked with @objc, and we can then call that from UIImageWriteToSavedPhotosAlbum()
 class ImageSaver: NSObject {
+    var successHandler: (() -> Void)?
+    var errorHandler: ((Error) -> Void)?
+    
     func writeToPhotoAlbum(image: UIImage) {
         UIImageWriteToSavedPhotosAlbum(image, self, #selector(saveCompleted), nil)
     }
 
     @objc func saveCompleted(_ image: UIImage, didFinishSavingWithError error: Error?, contextInfo: UnsafeRawPointer) {
-        print("Save finished!")
+        if let error = error {
+            errorHandler?(error)
+        } else {
+            successHandler?()
+        }
     }
 }
